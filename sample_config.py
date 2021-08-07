56bd8d86d41aa6c4a28a6411f0a5eb5f2a86f2b7
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-07 06:09:00 -0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -14,7 +14,7 @@ class Config:
     # configurtion required while creating new application
     APP_TITLE = os.environ.get("APP_TITLE", "usetgbot")
     APP_SHORT_NAME = os.environ.get("APP_SHORT_NAME", "usetgbot")
-    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/SDBOTs_inifinity")
+    APP_URL = os.environ.get("APP_URL", "https://telegram.dog/SDMyTgOrgBot")
     # these platform informations were obtained
     # on 27.01.2020 21:15:50 IST
     APP_PLATFORM = [
